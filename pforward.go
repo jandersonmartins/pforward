9ad5334a1b302ad9b4c50d35346e2c26e067c60c
Janderson Martins <jandersonmartins10@gmail.com>
2021-06-01 08:52:50 -0300

    implement Forward function

--- pforward.go
+++ pforward.go
@@ -1,5 +1,12 @@
 package pforward
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
 type ForwardCnf struct {
 	serverPort int32
 	destHost   string
@@ -7,4 +14,35 @@ type ForwardCnf struct {
 }
 
 func Forward(cnf ForwardCnf) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", cnf.serverPort))
+	if err != nil {
+		log.Fatalf("listener error: %v", err)
+	}
+	destAddress := fmt.Sprintf("%s:%d", cnf.destHost, cnf.destPort)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("connection error: %v", err)
+		} else {
+			go handleConnection(conn, destAddress)
+		}
+	}
+}
+
+func handleConnection(conn net.Conn, destAddress string) {
+	dest, err := net.Dial("tcp", destAddress)
+	defer conn.Close()
+	if err != nil {
+		log.Printf("dest (%s) conn error: %v", destAddress, err)
+	} else {
+		defer dest.Close()
+		go func() {
+			if _, err := io.Copy(dest, conn); err != nil {
+				log.Printf("can't copy to dest: %v", err)
+			}
+		}()
+		if _, err := io.Copy(conn, dest); err != nil {
+			log.Printf("can't copy data: %v", err)
+		}
+	}
 }
--- pforward_test.go
+++ pforward_test.go
@@ -11,37 +11,40 @@ import (
 	"time"
 )
 
+const (
+	TEST_SERVER_ADDRESS  = "127.0.0.1:9000"
+	TEST_SERVER_RESPONSE = "local server response"
+)
+
 func TestForward(t *testing.T) {
 	go localServer()
 
-	Forward(ForwardCnf{serverPort: 9001,
-		destHost: "localhost",
+	go Forward(ForwardCnf{serverPort: 9001,
+		destHost: "127.0.0.1",
 		destPort: 9000})
 
 	conn, err := net.DialTimeout(
 		"tcp",
-		"127.0.0.1:4000",
+		TEST_SERVER_ADDRESS,
 		time.Second*2)
 	if err != nil {
 		t.Fatalf("dial error: %v", err)
 	}
 
 	defer conn.Close()
-
 	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 	buff := bytes.Buffer{}
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(&buff)
-
 	io.Copy(writer, reader)
 
-	if !strings.Contains(buff.String(), "local server") {
+	if !strings.Contains(buff.String(), TEST_SERVER_RESPONSE) {
 		t.Errorf("connection was not forwarded")
 	}
 }
 
 func localServer() net.Listener {
-	listener, _ := net.Listen("tcp", "localhost:9000")
+	listener, _ := net.Listen("tcp", TEST_SERVER_ADDRESS)
 
 	defer listener.Close()
 	for {
@@ -50,7 +53,7 @@ func localServer() net.Listener {
 			buff := make([]byte, 1024)
 			conn.Read(buff)
 
-			conn.Write([]byte("local server"))
+			conn.Write([]byte(TEST_SERVER_RESPONSE))
 			conn.Close()
 		}(conn)
 	}
